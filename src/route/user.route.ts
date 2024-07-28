9217890a3fd3b3ad638b1592251b71607d86545e
BLACKHENZ29 <ayehenz29@gmail.com>
2024-07-28 14:09:54 +0100

    Retrieving User Route

--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { CreateUserDto } from "../dto/createUser.dto";
 import { validationMiddleware } from './../middleware/validation.middleware';
-import { createUser} from './../controller/user.controller';
+import { createUser, getUserById, getUsers} from './../controller/user.controller';
 
 
 const userRouter = Router();
 
 userRouter.post('/create-user', validationMiddleware(CreateUserDto), createUser);
+userRouter.get('/get-user/:id', getUserById);
+userRouter.get('/get-all-users', getUsers);
 
 export default userRouter;
