3c03d8877f5b7daf007cb2f959fc900fb2e94d4b
BLACKHENZ29 <ayehenz29@gmail.com>
2024-07-28 23:53:03 +0100

    Flutterwave payment service

--- src/service/payment.service.ts
+++ src/service/payment.service.ts
@@ -3,4 +3,5 @@ import { PayDto } from "../dto/payment.dto";
 
 export interface PaymentServices{
     makePayment(data: PayDto): Promise<{url: string}>;
+    flutterWave(data: PayDto): Promise<{url: string}>;
 }
--- src/service/serviceImpl/paymentImpl.ts
+++ src/service/serviceImpl/paymentImpl.ts
@@ -43,4 +43,46 @@ export class PaymentImpl implements PaymentServices{
         } return {url: res.data.data.authorization_url}
     }
 
+    async flutterWave(data: PayDto): Promise<{ url: string; }> {
+        const transaction = await db.transaction.create({
+            data: {
+                amount: data.amount,
+                user_id: data.user_id,
+                description: data.description,
+            }
+        });
+    
+        const ref = `REF-${transaction.id}`;
+        const actualAmount = data.amount;
+        const secret = process.env.FLUTTERWAVE_SECRET_KEY;
+        const url = 'https://api.flutterwave.com/v3/payments';
+        const headers = {
+            Authorization: `Bearer ${secret}`,
+        };
+    
+        const body = {
+            tx_ref: ref,
+            amount: actualAmount.toString(),
+            currency: "NGN",
+            redirect_url: "http://www.netflix.com",
+            customer: {
+                email: data.email,
+                name: data.name
+            },
+            customizations: {
+                title: "Payment for Subscription",
+                description: data.description,
+            }
+        };
+    
+        const res = await axios.post(url, body, {
+            headers,
+            validateStatus: () => true,
+        });
+        console.log(res);
+        if (res.status !== 200 || !res.data.status) {
+            throw new customError(409, res.data.message);
+        }
+        return { url: res.data.data.link };
+    }
 }
