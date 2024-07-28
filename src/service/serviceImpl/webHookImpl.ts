c27d49f9ecf988197f6f797876524ff7b366e6a6
BLACKHENZ29 <ayehenz29@gmail.com>
2024-07-28 23:56:05 +0100

    Flutterwave Webhook service implement

--- src/service/serviceImpl/webHookImpl.ts
+++ src/service/serviceImpl/webHookImpl.ts
@@ -5,7 +5,6 @@ import { db } from "../../utils/db.utils";
 import { webHookService } from "../webhook.service";
 
 export class webHookImpl implements webHookService {
-
   async handle(data: any): Promise<void> {
     try {
       if (data.event === "charge.success") {
@@ -37,16 +36,58 @@ export class webHookImpl implements webHookService {
     }
   }
 
-
   verify(signature: string, data: any): boolean | Promise<boolean> {
     const secret = process.env.PAYSTACK_SECRET_KEY;
     if (!secret) {
       throw new customError(500, "Your Secret Key is not set");
-    }   
+    }
     const hash = createHmac("sha512", secret)
       .update(JSON.stringify(data))
       .digest("hex");
     if (signature !== hash) {
+      throw new customError(409, "Invalid signature");
+    }
+    return true;
+  }
+
+  async handleFlutterwave(data: any): Promise<void> {
+    try {
+      console.log("Received payload:", data);
+      if (data.status === "successful") {
+        const ref = data.txRef;
+        const transaction_id = Number(ref.split("-")[1]);
+        const transaction = await db.transaction.findFirst({
+          where: {
+            id: transaction_id,
+          },
+        });
+        if (
+          transaction?.status === "successful" ||
+          transaction?.status === "failed"
+        ) {
+          return;
+        }
+        await db.transaction.update({
+          where: {
+            id: transaction_id,
+          },
+          data: {
+            status: "successful",
+          },
+        });
+      }
+    } catch (error) {
+      console.log("Error in updating");
+    }
+  }
+
+  verifyFlutterwave(signature: string): boolean | Promise<boolean> {
+    const secret = process.env.FLUTTERWAVE_SECRET_HASH;
+    if (!secret) {
+      throw new customError(500, "Your Secret Hash is not set");
+    }
+    console.log("Received signature:", signature);
+    if (signature !== secret) {
       throw new customError(409, "Invalid signature");
     }
     return true;
--- src/service/webhook.service.ts
+++ src/service/webhook.service.ts
@@ -1,4 +1,7 @@
 export interface webHookService {
     handle(data: any): Promise<void>
     verify(hash: string, data: any): Promise<boolean> | boolean;
+
+    handleFlutterwave(data: any): Promise<void>
+    verifyFlutterwave(hash: string): Promise<boolean> | boolean;
 }
