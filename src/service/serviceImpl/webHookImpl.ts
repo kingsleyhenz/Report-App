a108bc189ee3949e8c6ac3b067c1d09d9642eaef
BLACKHENZ29 <ayehenz29@gmail.com>
2024-07-28 23:48:58 +0100

    Paystack Webhook service implement

---
+++ src/service/serviceImpl/webHookImpl.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { createHmac } from "crypto";
+import { customError } from "../../error/customError";
+import { db } from "../../utils/db.utils";
+import { webHookService } from "../webhook.service";
+
+export class webHookImpl implements webHookService {
+
+  async handle(data: any): Promise<void> {
+    try {
+      if (data.event === "charge.success") {
+        const ref = data.data.reference as string;
+        const transaction_id = Number(ref.split("-")[1]);
+
+        const transaction = await db.transaction.findFirst({
+          where: {
+            id: transaction_id,
+          },
+        });
+        if (
+          transaction?.status === "successful" ||
+          transaction?.status === "failed"
+        ) {
+          return;
+        }
+        await db.transaction.update({
+          where: {
+            id: transaction_id,
+          },
+          data: {
+            status: "successful",
+          },
+        });
+      }
+    } catch (error) {
+      console.log("error in updating transaction");
+    }
+  }
+
+
+  verify(signature: string, data: any): boolean | Promise<boolean> {
+    const secret = process.env.PAYSTACK_SECRET_KEY;
+    if (!secret) {
+      throw new customError(500, "Your Secret Key is not set");
+    }   
+    const hash = createHmac("sha512", secret)
+      .update(JSON.stringify(data))
+      .digest("hex");
+    if (signature !== hash) {
+      throw new customError(409, "Invalid signature");
+    }
+    return true;
+  }
+}
